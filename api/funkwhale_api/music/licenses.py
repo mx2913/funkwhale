494f8a8eb0a913de258a18438d07eaa833f32d42
Renon <renon@disroot.org>
2018-09-05 14:13:40 +0200

    add licenses dict

---
+++ api/funkwhale_api/music/licenses.py
@@ -0,0 +1,50 @@
+LICENCES = {
+		'cc0': {
+			'name': 'Public domain',
+			'allow_modifications': True,
+			'allow_redistribution': True,
+			'url': 'https://creativecommons.org/publicdomain/zero/1.0/',
+			},
+		'cc-by': {
+			'name': 'Creative Commons - Attribution',
+			'allow_modifications': True,
+			'allow_redistribution': True,
+			'url': 'https://creativecommons.org/licenses/by/2.0/',
+			},
+		'cc-by-sa': {
+			'name': 'Creative Commons - Attribution - Share alike',
+			'allow_modifications': False,
+			'allow_redistribution': True,
+			'url': 'https://creativecommons.org/licenses/by-sa/4.0/',
+			},
+		'cc-by-nc': {
+			'name': 'Creative Commons - Attribution - Non-commercial',
+			'allow_modifications': True,
+			'allow_redistribution': True,
+			'url': 'https://creativecommons.org/licenses/by-nc/4.0/',
+			},
+		'cc-by-nc-sa': {
+			'name': 'Creative Commons - Attribution - Non-commercial - Share alike',
+			'allow_modifications': False,
+			'allow_redistribution': True,
+			'url': 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+			},
+		'cc-by-nd': {
+			'name': 'Creative Commons - Attribution - NoDerivatives',
+			'allow_modifications': False,
+			'allow_redistribution': True,
+			'url': 'https://creativecommons.org/licenses/by-nd/4.0/',
+			},
+		'cc-by-nc-nd': {
+			'name': 'Creative Commons - Attribution - Non-commercial - NoDerivatives',
+			'allow_modifications': False,
+			'allow_redistribution': True,
+			'url': 'https://creativecommons.org/licenses/by-nc-nd/4.0/',
+			},
+		'copyright': {
+			'name': 'Copyright',
+			'allow_modifications': False,
+			'allow_redistribution': False,
+			'url': '',
+			},
+		}
