65e42be97c2494c3ffce46ad1e697e7752253e19
Eliot Berriot <contact@eliotberriot.com>
2019-10-18 15:26:53 +0200

    Handle scope properly, minor improvements (icons, links)

--- front/src/components/library/Radios.vue
+++ front/src/components/library/Radios.vue
@@ -93,7 +93,7 @@
             v-for="radio in result.results"
             :key="radio.id"
             :custom-radio="radio"></radio-card>
-        </div>        
+        </div>
       </div>
       <div class="ui center aligned basic segment">
         <pagination
@@ -126,7 +126,8 @@ const FETCH_URL = "radios/radios/"
 export default {
   mixins: [OrderingMixin, PaginationMixin, TranslationsMixin],
   props: {
-    defaultQuery: { type: String, required: false, default: "" }
+    defaultQuery: { type: String, required: false, default: "" },
+    scope: { type: String, required: false, default: "all" },
   },
   components: {
     RadioCard,
@@ -188,10 +189,11 @@ export default {
       this.isLoading = true
       let url = FETCH_URL
       let params = {
+        scope: this.scope,
         page: this.page,
         page_size: this.paginateBy,
         name__icontains: this.query,
-        ordering: this.getOrderingAsString()
+        ordering: this.getOrderingAsString(),
       }
       logger.default.debug("Fetching radios")
       axios.get(url, { params: params }).then(response => {
