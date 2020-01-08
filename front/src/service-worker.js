4699a3a7f883c799c17620e73dc883be084d476c
Eliot Berriot <contact@eliotberriot.com>
2020-01-08 15:42:45 +0100

    Cache API/media files with service workers

--- front/src/service-worker.js
+++ front/src/service-worker.js
@@ -1,14 +1,20 @@
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
+workbox.loadModule('workbox-routing');
+workbox.loadModule('workbox-strategies');
+workbox.loadModule('workbox-expiration');
+
 self.addEventListener('message', (e) => {
   if (!e.data) {
     return;
   }
-
-  switch (e.data) {
+  console.log('[sw] received message', e.data)
+  switch (e.data.command) {
     case 'skipWaiting':
       self.skipWaiting();
       break;
+    case 'serverChosen':
+      self.registerServerRoutes(e.data.serverUrl)
     default:
       // NOOP
       break;
@@ -18,7 +24,7 @@ workbox.core.clientsClaim();
 
 // The precaching code provided by Workbox.
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-console.log('Files to be cached by service worker [before filtering]', self.__precacheManifest.length);
+console.log('[sw] Files to be cached [before filtering]', self.__precacheManifest.length);
 var excludedUrlsPrefix = [
   '/js/locale-',
   '/js/moment-locale-',
@@ -28,6 +34,45 @@ var excludedUrlsPrefix = [
 self.__precacheManifest = self.__precacheManifest.filter((e) => {
   return !excludedUrlsPrefix.some(prefix => e.url.startsWith(prefix))
 });
-console.log('Files to be cached by service worker [after filtering]', self.__precacheManifest.length);
+console.log('[sw] Files to be cached [after filtering]', self.__precacheManifest.length);
 // workbox.precaching.suppressWarnings(); // Only used with Vue CLI 3 and Workbox v3.
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+const router = new workbox.routing.Router();
+router.addCacheListener()
+router.addFetchListener()
+
+var registeredServerRoutes = []
+self.registerServerRoutes = (serverUrl) => {
+  console.log('[sw] Setting up API caching for', serverUrl)
+  registeredServerRoutes.forEach((r) => {
+    console.log('[sw] Unregistering previous API route...', r)
+    router.unregisterRoute(r)
+  })
+  if (!serverUrl) {
+    return
+  }
+  var regexReadyServerUrl = serverUrl.replace('.', '\\.')
+  registeredServerRoutes = []
+  var networkFirstRoutes = [
+    'api/v1/',
+    'media/',
+  ].map((path) => {
+    return new RegExp(regexReadyServerUrl + path)
+  })
+  var strategy = new workbox.strategies.NetworkFirst({
+    cacheName: "api-cache:" + serverUrl,
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 24 * 60 * 60 * 7,
+      }),
+    ]
+  })
+
+  networkFirstRoutes.forEach((r) => {
+    console.log('[sw] registering new API route...', r)
+    var route = new workbox.routing.RegExpRoute(r, strategy)
+    router.registerRoute(route)
+    registeredServerRoutes.push(route)
+  })
+}
