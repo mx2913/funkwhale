188d2ded283860fbfc39e267544289a3f170e156
Ciaran Ainsworth <ciaranainsworth@outlook.com>
2021-05-05 00:10:35 +0100

    Add very rudimentary mobile support

---
+++ front/src/components/audio/track/TrackMobileRow.vue
@@ -0,0 +1,165 @@
+<template>
+  <div
+    :class="[
+      { active: currentTrack && track.id === currentTrack.id },
+      'track-row row mobile',
+    ]"
+  >
+    <div class="actions one wide left floated column">
+      <play-indicator
+        v-if="
+          !$store.state.player.isLoadingAudio &&
+          currentTrack &&
+          isPlaying &&
+          track.id === currentTrack.id
+        "
+      >
+      </play-indicator>
+      <button
+        v-else-if="
+          currentTrack &&
+          !isPlaying &&
+          track.id === currentTrack.id
+        "
+        class="ui really tiny basic icon button play-button paused"
+      >
+        <i class="pause icon" />
+      </button>
+      <span class="track-position" v-else-if="showPosition">
+        {{ prettyPosition(track.position) }}
+      </span>
+    </div>
+    <div
+      v-if="showArt"
+      @click="activateTrack(track, index)"
+      class="image left floated column"
+    >
+      <img
+        alt=""
+        class="ui artist-track mini image"
+        v-if="
+          track.album && track.album.cover && track.album.cover.urls.original
+        "
+        v-lazy="
+          $store.getters['instance/absoluteUrl'](
+            track.album.cover.urls.medium_square_crop
+          )
+        "
+      />
+      <img
+        alt=""
+        class="ui artist-track mini image"
+        v-else
+        src="../../../assets/audio/default-cover.png"
+      />
+    </div>
+    <div
+      @click="activateTrack(track, index)"
+      class="content ellipsis left floated column"
+    >
+      <p class="track-title mobile">{{ track.title }}</p>
+      <p class="track-meta mobile">
+        {{ track.artist.name }} ･
+        <human-duration
+          v-if="track.uploads[0] && track.uploads[0].duration"
+          :duration="track.uploads[0].duration"
+        ></human-duration>
+      </p>
+    </div>
+    <div
+      v-if="$store.state.auth.authenticated"
+      class="meta right floated column mobile"
+    >
+      <track-favorite-icon
+        class="tiny"
+        :border="false"
+        :track="track"
+      ></track-favorite-icon>
+    </div>
+    <div
+      role="button"
+      aria-label="Show Details"
+      class="modal-button right floated column mobile"
+    >
+      <i class="ellipsis vertical icon" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PlayIndicator from "@/components/audio/track/PlayIndicator";
+import { mapActions, mapGetters } from "vuex";
+import TrackFavoriteIcon from "@/components/favorites/TrackFavoriteIcon";
+
+export default {
+  props: {
+    tracks: Array,
+    showAlbum: { type: Boolean, required: false, default: true },
+    showArtist: { type: Boolean, required: false, default: true },
+    trackOnly: { type: Boolean, required: false, default: false },
+    showPosition: { type: Boolean, required: false, default: false },
+    showArt: { type: Boolean, required: false, default: true },
+    search: { type: Boolean, required: false, default: false },
+    filters: { type: Object, required: false, default: null },
+    nextUrl: { type: String, required: false, default: null },
+    displayActions: { type: Boolean, required: false, default: true },
+    showDuration: { type: Boolean, required: false, default: true },
+    index: { type: Number, required: true },
+    track: { type: Object, required: true },
+  },
+
+  components: {
+    PlayIndicator,
+    TrackFavoriteIcon,
+  },
+  computed: {
+    ...mapGetters({
+      currentTrack: "queue/currentTrack",
+    }),
+
+    isPlaying() {
+      return this.$store.state.player.playing;
+    },
+  },
+
+  methods: {
+    replacePlay(tracks, trackIndex) {
+      this.$store.dispatch("queue/clean");
+      this.$store.dispatch("queue/appendMany", { tracks: tracks }).then(() => {
+        this.$store.dispatch("queue/currentIndex", trackIndex);
+      });
+    },
+
+    prettyPosition(position, size) {
+      var s = String(position);
+      while (s.length < (size || 2)) {
+        s = "0" + s;
+      }
+      return s;
+    },
+
+    activateTrack(track, index) {
+      if (
+        this.currentTrack &&
+        this.isPlaying &&
+        track.id === this.currentTrack.id
+      ) {
+        this.pausePlayback();
+      } else if (
+        this.currentTrack &&
+        !this.isPlaying &&
+        track.id === this.currentTrack.id
+      ) {
+        this.resumePlayback();
+      } else {
+        this.replacePlay(this.tracks, index);
+      }
+    },
+
+    ...mapActions({
+      resumePlayback: "player/resumePlayback",
+      pausePlayback: "player/pausePlayback",
+    }),
+  },
+};
+</script>
