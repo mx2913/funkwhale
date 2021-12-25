14911b9dbad83855d7a1dc7393e8dac231bbd21e
Georg Krause <mail@georg-krause.net>
2021-12-26 00:13:32 +0100

    Fix swagger oauth

--- api/config/settings/local.py
+++ api/config/settings/local.py
@@ -111,6 +111,27 @@ SPECTACULAR_SETTINGS = {
         "funkwhale_api.common.authentication.OAuth2Authentication",
         "funkwhale_api.common.authentication.ApplicationTokenAuthentication",
     ],
+    "SERVERS": [
+        {"url": "https://demo.funkwhale.audio", "description": "Demo Server"},
+        {
+            "url": "https://funkwhale.audio",
+            "description": "Read server with real content",
+        },
+        {
+            "url": "https://{domain}",
+            "description": "Custom server",
+            "variables": {
+                "domain": {
+                    "default": "yourdomain",
+                    "description": "Your Funkwhale Domain",
+                },
+                "protocol": {"enum": ["http", "https"], "default": "https"},
+            },
+        },
+    ],
+    "OAUTH2_FLOWS": ["authorizationCode"],
+    "OAUTH2_AUTHORIZATION_URL": "/authorize",
+    "OAUTH2_TOKEN_URL": "/api/v1/oauth/token/",
 }
 
 if env.bool("WEAK_PASSWORDS", default=False):
@@ -122,3 +143,31 @@ MIDDLEWARE = (
     "funkwhale_api.common.middleware.ProfilerMiddleware",
     "funkwhale_api.common.middleware.PymallocMiddleware",
 ) + MIDDLEWARE
+
+from drf_spectacular.contrib.django_oauth_toolkit import OpenApiAuthenticationExtension
+
+
+class CustomOAuthExt(OpenApiAuthenticationExtension):
+    target_class = "funkwhale_api.common.authentication.OAuth2Authentication"
+    name = "oauth2"
+
+    def get_security_definition(self, auto_schema):
+        from oauth2_provider.scopes import get_scopes_backend
+
+        from drf_spectacular.settings import spectacular_settings
+
+        flows = {}
+        for flow_type in spectacular_settings.OAUTH2_FLOWS:
+            flows[flow_type] = {}
+            if flow_type in ("implicit", "authorizationCode"):
+                flows[flow_type][
+                    "authorizationUrl"
+                ] = spectacular_settings.OAUTH2_AUTHORIZATION_URL
+            if flow_type in ("password", "clientCredentials", "authorizationCode"):
+                flows[flow_type]["tokenUrl"] = spectacular_settings.OAUTH2_TOKEN_URL
+            if spectacular_settings.OAUTH2_REFRESH_URL:
+                flows[flow_type]["refreshUrl"] = spectacular_settings.OAUTH2_REFRESH_URL
+            scope_backend = get_scopes_backend()
+            flows[flow_type]["scopes"] = scope_backend.get_all_scopes()
+
+        return {"type": "oauth2", "flows": flows}
