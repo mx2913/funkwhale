1472ade3bf8737387dbd5be69419e57e892e5f9f
JuniorJPDJ <git@juniorjpdj.pl>
2022-11-25 22:10:39 +0100

    fix(ci): fix CI `build_documentation` job swagger-UI generation and remove leftovers

--- docs/build_swagger.sh
+++ docs/build_swagger.sh
@@ -2,13 +2,11 @@
 
 set -eux
 
-SWAGGER_VERSION="4.1.3"
+SWAGGER_VERSION="4.15.5"
 TARGET_PATH=${TARGET_PATH-"swagger"}
 
 rm -rf "$TARGET_PATH" /tmp/swagger-ui
 git clone --branch="v$SWAGGER_VERSION" --depth=1 "https://github.com/swagger-api/swagger-ui.git" /tmp/swagger-ui
 
 mv /tmp/swagger-ui/dist "$TARGET_PATH"
-cp schema.yml "$TARGET_PATH"
-cp -r api "$TARGET_PATH/api"
-sed -i "s,https://petstore.swagger.io/v2/swagger.json,schema.yml,g" "$TARGET_PATH/index.html"
+sed -i "s#https://petstore.swagger.io/v2/swagger.json#https://dev.funkwhale.audio/funkwhale/funkwhale/-/jobs/artifacts/$CI_COMMIT_BRANCH/raw/docs/schema.yml?job=build_openapi_schema#g" "$TARGET_PATH/swagger-initializer.js"
