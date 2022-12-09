1e615bde7372df0f77b1aec91d59efe2218d4b3e
JuniorJPDJ <git@juniorjpdj.pl>
2022-12-09 17:08:15 +0000

    Update docs/build_swagger.sh

--- docs/build_swagger.sh
+++ docs/build_swagger.sh
@@ -11,8 +11,8 @@ trap 'rm -rf "$tmpdir"' EXIT
 pushd "$tmpdir"
 wget "https://github.com/swagger-api/swagger-ui/archive/refs/tags/v$SWAGGER_VERSION.tar.gz" -O swagger-ui.tgz
 tar -xzf swagger-ui.tgz
-mv ./*/dist "$TARGET_PATH"
 popd
+mv "$tmpdir/"*/dist "$TARGET_PATH"
 cp schema.yml "$TARGET_PATH"
 
 sed -i "s#https://petstore.swagger.io/v2/swagger.json#schema.yml#g" "$TARGET_PATH/swagger-initializer.js"
