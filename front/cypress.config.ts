caa6a26c77973e85acdcf0d546944ffa7bd58fc4
Kasper Seweryn <github@wvffle.net>
2023-06-19 23:02:40 +0200

    refactor(cypress): remove unused plugins section
    Part-of: <https://dev.funkwhale.audio/funkwhale/funkwhale/-/merge_requests/1795>

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   chromeWebSecurity: false,
   e2e: {
-    // NOTE: Set up plugins
-    // setupNodeEvents (on, config) {
-    //
-    // },
     baseUrl: 'https://demo.funkwhale.audio'
   }
 })
