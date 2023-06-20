6f282f984de3769ca0f28d605a6260bcc0a47466
Kasper Seweryn <github@wvffle.net>
2023-06-20 11:37:47 +0200

    test: remove useless navigation to /login while logging in via command
    Part-of: <https://dev.funkwhale.audio/funkwhale/funkwhale/-/merge_requests/1795>

--- front/cypress/support/commands.ts
+++ front/cypress/support/commands.ts
@@ -1,12 +1,7 @@
-// Currently we cannot login purely programmatically, so we need to use the
-// graphical login until the vue3 branch is merged
 Cypress.Commands.add('login', () => {
   cy.fixture('testuser.json').then(({ username, password }) => {
     // We need to request a page that sets the csrf cookie
     cy.request('/api/v1/instance/nodeinfo/2.0/')
-
-    // Then we can login
-    cy.visit('/login')
 
     cy.getCookie('csrftoken').then(($cookie) => {
       cy.request({
