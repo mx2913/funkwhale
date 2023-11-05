0e516949aae0fb387b7a1067654f33425ea9a9db
Petitminion <petitminion@riseup.net>
2023-11-06 00:09:44 +0100

    lint

--- front/src/views/admin/Settings.vue
+++ front/src/views/admin/Settings.vue
@@ -57,7 +57,7 @@ const groups = computed(() => [
     settings: [
       { name: 'music__transcoding_enabled' },
       { name: 'music__transcoding_cache_duration' },
-      { name: 'music__only_allow_musicbrainz_tagged_files' },
+      { name: 'music__only_allow_musicbrainz_tagged_files' }
 
     ]
   },
