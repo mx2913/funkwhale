443a535758ef2f233c2609fe4c434e00b7f300a8
Kasper Seweryn <github@wvffle.net>
2024-01-21 14:28:06 +0100

    feat: add missing upload views

--- front/src/ui/pages/upload/all.vue
+++ front/src/ui/pages/upload/all.vue
@@ -1,13 +1,50 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
-import type { UploadGroupEntry } from '~/ui/stores/upload';
+import { computed } from 'vue';
+import { bytesToHumanSize } from '~/ui/composables/bytes';
+import { useUploadsStore, type UploadGroupEntry } from '~/ui/stores/upload';
+import CoverArt from '~/ui/components/CoverArt.vue'
 
-const allTracks: UploadGroupEntry[] = [
-  
-]
+interface Recording {
+  guid: string
+  title: string
+  artist: string
+  album: string
+  uploadDate: Date
+  format: string
+  size: string
+  metadata: UploadGroupEntry['metadata']
+}
+
+const intl = new Intl.DateTimeFormat('en', {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric'
+})
+
+// TODO: Fetch tracks from server
+const uploads = useUploadsStore()
+const allTracks = computed<Recording[]>(() => {
+  return uploads.uploadGroups.flatMap(group => group.queue.map<Recording>((entry) => ({
+    guid: entry.id,
+    title: entry.metadata?.tags.title || 'Unknown title',
+    artist: entry.metadata?.tags.artist || 'Unknown artist',
+    album: entry.metadata?.tags.album || 'Unknown album',
+    uploadDate: group.createdAt,
+    format: 'flac',
+    size: bytesToHumanSize(entry.file.size),
+    metadata: entry.metadata
+  })))
+})
 
 const columns = [
-  { key: '', label: '' }
+  { key: '>index', label: '#' },
+  { key: 'title', label: 'Title' },
+  { key: 'artist', label: 'Artist' },
+  { key: 'album', label: 'Album' },
+  { key: 'uploadDate', label: 'Upload date' },
+  { key: 'format', label: 'Format' },
+  { key: 'size', label: 'Size' }
 ]
 </script>
 
@@ -18,8 +55,20 @@ const columns = [
     <h3>There is no file in your library</h3>
     <p>Try uploading some before coming back here!</p>
   </div>
-  <FwTable :rows="allTracks">
-
+  <FwTable v-else 
+    id-key="guid" 
+    :columns="columns" 
+    :rows="allTracks"
+  >
+    <template #col-title="{ row, value }">
+      <div class="flex items-center">
+        <CoverArt :src="row.metadata" class="mr-2" />
+        {{ value }}
+      </div>
+    </template>
+    <template #col-upload-date="{ value }">
+      {{ intl.format(value) }}
+    </template>
   </FwTable>
 </template>
 
